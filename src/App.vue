d7ed0e30ea9d2039a4864f69d909204b1a44d5bd
cayonina <cayo.nitar@gmail.com>
2020-04-23 13:23:33 -0400

    Se le puso el formulario con el titulo y texto

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+   <h3>{{ titulo }}</h3>
+   <div class="form">
+     <div class="form-group">
+       <label>Titulo</label>
+       <input class="form-control" type="text" v-model="nota.titulo">
+     </div>
+     <div class="form-group">
+       <label>Texto</label>
+       <textarea class="form-control" v-model="nota.texto"></textarea>
+     </div>
+   </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      titulo: 'Gestion de Notas',
+      nota:{
+        titulo:'',
+        texto:''
+      }
+    }
+  }
+}
+</script>
+
+<style>
+  .form{
+    text-align: left;
+  }
+</style>
