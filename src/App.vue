326d57c3e8b47fb7c6efe8f387d41925aafd8b96
cayonina <cayo.nitar@gmail.com>
2020-04-24 17:43:41 -0400

    Se termino el gestor de notas con metodos de agregar y eliminar notas

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,19 @@
      <div class="form-group">
        <label>Texto</label>
        <textarea class="form-control" v-model="nota.texto"></textarea>
+     </div>                     
+     <button class="btn btn-primary" @click="agregarNota">Enviar</button>
+   </div>
+   <div class="col-sm-12">
+     <div class="col-sm-4 nota" v-for="(nota, index) in notas">
+       <div class="card">
+         <div class="card-block">
+           <div class="card-title">{{nota.titulo}}</div>
+           <div class="card-subtitle mb-2 text-muted">{{nota.fecha}}</div>
+           <p class="card-text">{{nota.texto}}</p>
+         </div>
+         <button class="close" v-on:click="eliminarNota(index)">&times; </button>
+       </div>
      </div>
    </div>
   </div>
@@ -23,7 +36,31 @@ export default {
       nota:{
         titulo:'',
         texto:''
-      }
+      },
+      notas:[
+        {
+          titulo:'Ir al cine',
+          texto:'Examinar las peliculas de estreno',
+          fecha: new Date(Date.now()).toLocaleDateString()
+        }
+      ]
+    }
+  },
+  methods:{
+    agregarNota:function(){
+      let {texto, titulo}= this.nota;
+      this.notas.push({
+      texto,
+      titulo,
+      fecha: new Date(Date.now()).toLocaleString()
+      });
+      texto='';
+      titulo='';
+      this.nota={texto,titulo};
+      
+    },
+    eliminarNota:function(index){
+      this.notas.splice(index,1);
     }
   }
 }
@@ -32,5 +69,16 @@ export default {
 <style>
   .form{
     text-align: left;
+  }
+  .card{
+    text-align: left;
+    border: 1px solid #2c2e50;
+    border-radius: 4px;
+    padding-left: 8px;
+    padding-right: 8px;
+
+  }
+  .nota{
+    padding: 5px;
   }
 </style>
